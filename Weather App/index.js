7351083ae7d7571f5c8289b75d07ac0ff3d1f21a
JoachimTerepo <joachimterepo6@gmail.com>
2023-06-22 06:57:08 +1200

    Final Product, need bug fixing.

--- Weather App/index.js
+++ Weather App/index.js
@@ -12,8 +12,10 @@ search.addEventListener('click', () =>{
     if (city === ''){
         return;
     }
-    else{
-        fetch('https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${APIKey}').then(response => response.json()).then (json => {
+
+        fetch('https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${APIKey}')
+        .then(response => response.json())
+        .then (json => {
 
             if (json.cod === '404'){
                 container.style.height = '400px';
@@ -21,11 +23,47 @@ search.addEventListener('click', () =>{
                 weatherDetails.style.display = 'none';
                 error404.style.display = 'block';
                 error404.classList.add('fadeIn');
+                return;
             }
-        })
-    }
 
-        
+            error404.style.display = 'none';
+            error404.classList.add('fadeIn');
 
+            const image = document.querySelector('.weather-box img');
+            const temperature = document.querySelector('.weather-box .temperature');
+            const description = document.querySelector('weather-box .description');
+            const humidity = document.querySelector('weather-details .humidty span');
+            const wind = document.querySelector('.weather-details .wind span');
 
-})
+            switch (json.weather[0].main){
+                case 'Clear':
+                    image.src = 'images/clear.png';
+                    break;
+                case 'Rain':
+                    image.src = 'images/rain.png';
+                    break;
+                case 'Snow':
+                    image.src = 'images/snow.png';
+                    break;
+                case 'Clouds':
+                    image.src = 'images/clouds.png';
+                    break;
+                case 'Haze':
+                    image.src = 'images/mist.png';
+                    break;
+                default:
+                    image.src = '';
+            }
+
+            temperature.innerHTML = '${parseInt(json.main.temp)}<span>°C</span>';
+            description.innerHTML = '${json.weather[0].description}';
+            humidity.innerHTML = '${json.main.humidity}%';
+            wind.innerHTML = '${parse(json.wind.speed)}KM/h';
+
+            weatherBox.style.display = '';
+            weatherDetails.style.display = '';
+            weatherBox.classList.add('fadeIn');
+            weatherDetails.classList.add('fadeIn');
+            container.style.height = '590px'
+        });
+});
