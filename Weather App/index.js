ee327716136d900dcec63e7354bee245baf0c5ed
JoachimTerepo <joachimterepo6@gmail.com>
2023-06-22 07:25:01 +1200

    Fixed bugs, program is working...

--- Weather App/index.js
+++ Weather App/index.js
@@ -1,19 +1,19 @@
 const container = document.querySelector('.container');
-const search = document.querySelector('.search-box button');
+const search = document.querySelector('.search-bar button');
 const weatherBox = document.querySelector('.weather-box');
-const weatherDetails = document.querySelector('.weatherDetails');
+const weatherDetails = document.querySelector('.weather-details');
 const error404 = document.querySelector('.not-found');
 
-search.addEventListener('click', () =>{
+search.addEventListener('click', () => {
 
     const APIKey = '84d2c1ed4f695160ec4781a6ab4d9084';
-    const city = document.querySelector('.search-box input').value;
+    const city = document.querySelector('.search-bar input').value;
 
     if (city === ''){
         return;
     }
 
-        fetch('https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${APIKey}')
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${APIKey}`)
         .then(response => response.json())
         .then (json => {
 
@@ -31,8 +31,8 @@ search.addEventListener('click', () =>{
 
             const image = document.querySelector('.weather-box img');
             const temperature = document.querySelector('.weather-box .temperature');
-            const description = document.querySelector('weather-box .description');
-            const humidity = document.querySelector('weather-details .humidty span');
+            const description = document.querySelector('.weather-box .description');
+            const humidity = document.querySelector('.weather-details .humidity span');
             const wind = document.querySelector('.weather-details .wind span');
 
             switch (json.weather[0].main){
@@ -46,7 +46,7 @@ search.addEventListener('click', () =>{
                     image.src = 'images/snow.png';
                     break;
                 case 'Clouds':
-                    image.src = 'images/clouds.png';
+                    image.src = 'images/cloud.png';
                     break;
                 case 'Haze':
                     image.src = 'images/mist.png';
@@ -55,10 +55,10 @@ search.addEventListener('click', () =>{
                     image.src = '';
             }
 
-            temperature.innerHTML = '${parseInt(json.main.temp)}<span>°C</span>';
-            description.innerHTML = '${json.weather[0].description}';
-            humidity.innerHTML = '${json.main.humidity}%';
-            wind.innerHTML = '${parse(json.wind.speed)}KM/h';
+            temperature.innerHTML = `${parseInt(json.main.temp)}<span>°C</span>`;
+            description.innerHTML = `${json.weather[0].description}`;
+            humidity.innerHTML = `${json.main.humidity}%`;
+            wind.innerHTML = `${parseInt(json.wind.speed)}Km/h`;
 
             weatherBox.style.display = '';
             weatherDetails.style.display = '';
